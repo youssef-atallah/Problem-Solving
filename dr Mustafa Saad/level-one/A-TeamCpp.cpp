af2163c0a76ece794840bfe593fc915a7b0aa813
Y atallah <youssefajoo3@gmail.com>
2024-03-18 19:25:05 -0700

    A_Team C++ & Java

---
+++ dr Mustafa Saad/level-one/A-TeamCpp.cpp
@@ -0,0 +1,36 @@
+/*
+ Problem link : https://codeforces.com/contest/231/problem/A
+
+   11- A. Anton and Danik
+    status link : https://codeforces.com/contest/231/submission/223721470
+*/
+#include <iostream>
+using namespace std ;
+
+int main(){
+  short n = 1 , r = 0 , result = 0;
+
+  cin >> n ;
+  int t[n][3] ;
+
+  for(int i = 0 ; i < n ; ++i){
+    r = 0 ;
+    for(int j = 0 ; j < 3 ; ++j){   // team m is 3
+      cin >> t[i][j] ;
+    }
+
+    for(int j = 0 ; j < 3 ; ++j){
+      if(t[i][j] == 1)       // calc Number of approvals
+        r += 1 ;
+    }
+
+    if(r >= 2)  
+      result += 1 ;  // if at least two are sure and agree will solve the p
+    
+  } 
+
+  cout << result ;
+
+
+  return 0;
+}
---
+++ dr Mustafa Saad/level-one/A_Team.java
@@ -0,0 +1,37 @@
+/* Java
+ Problem link : https://codeforces.com/contest/231/problem/A
+
+   11- A. Anton and Danik
+    status link : https://codeforces.com/contest/231/submission/252112186
+*/
+
+import java.util.Scanner;
+
+public class A_Team {
+    
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in) ;        
+
+        short n = 1 , result = 0;
+        n = in.nextShort() ;  // problems numbers 
+
+        int[][] team = new int[n][3] ;   // 3 in the team
+
+        for (int i = 0; i < n; i++) {
+            short tmp = 0 ;
+            for (int j = 0; j < 3; j++) {
+
+                team[i][j] = in.nextInt() ;
+
+                if(team[i][j] == 1)    // calc Number of approvals
+                    tmp++ ; 
+                
+            }
+            if (tmp >= 2)   // if at least two are sure and agree will solv the p
+                result++ ;
+
+        }
+
+        System.out.println(result);
+    }
+}
